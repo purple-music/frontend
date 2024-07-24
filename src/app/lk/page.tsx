4cded827d5086b6877d897a9aaca280ba238f2ef
KhoDis <khodisdev@gmail.com>
2024-07-24 17:49:05 +0300

    refactor: move account pages to lk

---
+++ src/app/lk/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="mx-auto flex flex-col gap-6 py-12">
+      <h1 className="text-4xl font-bold">Дашборд</h1>
+      <p>Здесь будут записи конкретного пользователя</p>
+    </div>
+  );
+}
