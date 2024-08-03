0737bf17c74de341022f1fabd42209d1787c84e2
KhoDis <khodzhayarov.a@gmail.com>
2024-08-03 20:36:04 +0300

    feat: improve styling for login form

--- src/ui/auth/login-form.tsx
+++ src/ui/auth/login-form.tsx
@@ -20,7 +20,7 @@ export default function LoginForm() {
       action={formAction}
       className="card bg-base-100"
     >
-      <section className="card-body">
+      <section className="card-body items-stretch">
         <h1 className="card-title mb-4 text-2xl">Login</h1>
 
         {/* Email Field */}
@@ -42,7 +42,7 @@ export default function LoginForm() {
         {/* Submit Button */}
         <button
           type="submit"
-          className="btn btn-primary w-full rounded px-4 py-2 text-white"
+          className="btn btn-primary text-white"
           disabled={isPending}
         >
           {isPending ? "Logging in..." : "Login"}
@@ -50,6 +50,8 @@ export default function LoginForm() {
 
         {/* General Message */}
         {message && <p className="mt-4 text-red-500">{message}</p>}
+
+        <div className="divider">or</div>
 
         <Social />
       </section>
