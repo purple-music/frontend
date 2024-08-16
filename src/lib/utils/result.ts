ee46453adda094b541a22cb0f02e1ec966a979a9
KhoDis <khodzhayarov.a@gmail.com>
2024-08-16 17:22:00 +0300

    feat: add result type helper

---
+++ src/lib/utils/result.ts
@@ -0,0 +1,19 @@
+type SuccessType<S> = {
+  type: "success";
+  content: S;
+};
+
+type ErrorType<E> = {
+  type: "error";
+  error: E;
+};
+
+export type Result<S, E> = SuccessType<S> | ErrorType<E>;
+
+export function success<S>(content: S): SuccessType<S> {
+  return { type: "success", content };
+}
+
+export function error<E>(error: E): ErrorType<E> {
+  return { type: "error", error };
+}
