a0b0701d6bddb1f974355a382d217a2d154189fb
KhoDis <khodzhayarov.a@gmail.com>
2024-09-02 20:02:55 +0300

    chore: add secret and remove nocodb

--- src/auth.ts
+++ src/auth.ts
@@ -89,5 +89,6 @@ export const { auth, signIn, signOut, handlers } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   trustHost: true,
+  secret: process.env.AUTH_SECRET,
   ...authConfig,
 });
