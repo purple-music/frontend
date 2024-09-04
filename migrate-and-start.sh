173cd379cc29f466f1b0ec9666224f0b113cb022
KhoDis <khodzhayarov.a@gmail.com>
2024-09-04 16:45:04 +0300

    chore: remove prisma generation in the last build stage

--- migrate-and-start.sh
+++ migrate-and-start.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-DATABASE_URL="postgresql://purple:music_purple@db:5432/music?sslmode=disable" npx --yes prisma generate
 DATABASE_URL="postgresql://purple:music_purple@db:5432/music?sslmode=disable" npx --yes prisma migrate deploy
 DATABASE_URL="postgresql://purple:music_purple@db:5432/music?sslmode=disable" node server.js
