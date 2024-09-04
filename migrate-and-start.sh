65bc35af7b287bb324bef2f2d5c1dbaa691b99f2
KhoDis <khodzhayarov.a@gmail.com>
2024-09-04 17:03:08 +0300

    chore: update prisma version

--- migrate-and-start.sh
+++ migrate-and-start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-DATABASE_URL="postgresql://purple:music_purple@db:5432/music?sslmode=disable" npx --yes prisma migrate deploy
+DATABASE_URL="postgresql://purple:music_purple@db:5432/music?sslmode=disable" npx prisma migrate deploy
 DATABASE_URL="postgresql://purple:music_purple@db:5432/music?sslmode=disable" node server.js
