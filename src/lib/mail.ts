f9c5113d83c221e1802fc1813527326f5b19bda2
KhoDis <khodzhayarov.a@gmail.com>
2024-09-06 20:33:59 +0300

    chore: add domain name for resend

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -2,6 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const host = process.env.AUTH_URL;
+const domain = process.env.DOMAIN;
 
 export async function sendVerificationEmail(email: string, token: string) {
   const confirmLink = `${host}/auth/new-verification?token=${token}`;
@@ -10,13 +11,14 @@ export async function sendVerificationEmail(email: string, token: string) {
 
   try {
     const result = await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: `onboarding@${domain}`,
       to: email,
       subject: "Confirm your email",
       html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
     });
 
     console.log("Email sent", result);
+    // TODO: audit logs and erros (result = { data, error })
   } catch (error) {
     console.log("Error sending email", error);
   }
@@ -26,7 +28,7 @@ export async function sendPasswordResetEmail(email: string, token: string) {
   const confirmLink = `${host}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: `onboarding@${domain}`,
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${confirmLink}">here</a> to reset password.</p>`,
