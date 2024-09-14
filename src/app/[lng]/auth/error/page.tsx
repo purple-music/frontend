ac6d37bacf629f4032cba32fbfa27e3b5727218a
KhoDis <khodzhayarov.a@gmail.com>
2024-09-14 20:59:33 +0300

    refactor: move error card to components

--- src/app/[lng]/auth/error/page.tsx
+++ src/app/[lng]/auth/error/page.tsx
@@ -1,20 +1,4 @@
-import { AuthCard } from "@/components/auth-card/auth-card";
-import { AuthCardTitle } from "@/components/auth-card/auth-card-title";
-import { AuthFooterAction } from "@/components/auth-card/auth-footer-action";
-import { FaExclamationTriangle } from "react-icons/fa";
-
-function ErrorCard() {
-  return (
-    <AuthCard>
-      <AuthCardTitle title={"Error!"} />
-      Something went wrong...
-      <div className="flex w-full items-center justify-center">
-        <FaExclamationTriangle />
-      </div>
-      <AuthFooterAction href="/auth/login" label="Go back to login page" />
-    </AuthCard>
-  );
-}
+import { ErrorCard } from "@/components/auth/error-card";
 
 export default function Page() {
   return <ErrorCard />;
---
+++ src/components/auth/error-card.tsx
@@ -0,0 +1,17 @@
+import { AuthCard } from "@/components/auth-card/auth-card";
+import { AuthCardTitle } from "@/components/auth-card/auth-card-title";
+import { AuthFooterAction } from "@/components/auth-card/auth-footer-action";
+import { FaExclamationTriangle } from "react-icons/fa";
+
+export function ErrorCard() {
+  return (
+    <AuthCard>
+      <AuthCardTitle title={"Error!"} />
+      Something went wrong...
+      <div className="flex w-full items-center justify-center">
+        <FaExclamationTriangle />
+      </div>
+      <AuthFooterAction href="/auth/login" label="Go back to login page" />
+    </AuthCard>
+  );
+}
