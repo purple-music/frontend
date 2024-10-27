4d953c10e7f04446e9bbc6364fee75e513556ef9
KhoDis <khodzhayarov.a@gmail.com>
2024-10-27 14:10:40 +0300

    fix: fix typo in the component name

--- src/components/atoms/Typography/Typography.stories.ts
+++ src/components/atoms/Typography/Typography.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 
-import Typograhy from "./Typograhy";
+import Typograhy from "./Typography";
 
 const meta = {
   title: "atoms/Typograhy",
