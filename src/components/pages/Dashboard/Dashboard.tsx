450b5123f477475890be9a048e1fee54a6bafe88
KhoDis <khodisdev@gmail.com>
2024-11-01 10:53:30 +0300

    feat: add template for dashboard page

---
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,15 @@
+import { Content } from "next/font/google";
+import Image from "next/image";
+import { ReactNode, useState } from "react";
+import { FaBook, FaMusic } from "react-icons/fa";
+import { FaHouse } from "react-icons/fa6";
+
+import Typography from "@/components/atoms/Typography/Typography";
+import Sidebar from "@/components/organisms/Sidebar/Sidebar";
+import MyTemplate from "@/components/templates/MyTemplate/MyTemplate";
+
+interface DashboardPageProps {}
+
+const DashboardPage = ({}: DashboardPageProps) => {
+  return undefined;
+};
