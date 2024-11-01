aef12525071d0708b9f3a62b29563e0a407c4a25
KhoDis <khodisdev@gmail.com>
2024-11-01 17:59:00 +0300

    fix: update sizes of the divider

--- src/components/atoms/Divider/Divider.tsx
+++ src/components/atoms/Divider/Divider.tsx
@@ -12,9 +12,9 @@ const Divider = ({ children, direction = "horizontal" }: DividerProps) => {
       className={clsx(
         "self-stretch whitespace-nowrap items-center before:bg-outline-variant after:bg-outline-variant flex before:flex-1 after:flex-1",
         {
-          "flex-row w-full h-4 my-4 before:h-0.5 after:h-0.5 before:w-full after:w-full":
+          "flex-row w-full h-4 my-2 before:h-[1px] after:h-[1px] before:w-full after:w-full":
             direction === "horizontal",
-          "flex-col w-4 h-full mx-4 before:h-full after:h-full before:w-0.5 after:w-0.5":
+          "flex-col w-4 h-full mx-2 before:h-full after:h-full before:w-[1px] after:w-[1px]":
             direction === "vertical",
           "gap-2": children,
         },
