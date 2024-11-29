c1954e55b13e024585b5356339f5b595807f4547
KhoDis <khodisdev@gmail.com>
2024-11-29 20:38:54 +0300

    feat: style booking calendar sizes

--- src/components/molecules/BookingCalendar/BookingCalendar.tsx
+++ src/components/molecules/BookingCalendar/BookingCalendar.tsx
@@ -1,26 +1,51 @@
+import React from "react";
 import {
   Button,
   Calendar,
   CalendarCell,
   CalendarGrid,
+  CalendarGridBody,
+  CalendarGridHeader,
+  CalendarHeaderCell,
   Heading,
+  HeadingContext,
+  HeadingProps,
+  useContextProps,
 } from "react-aria-components";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
-import { Booking } from "../../../app/[locale]/my/booking/_components/Booking";
+import Typography from "@/components/atoms/Typography/Typography";
 
 const BookingCalendar = () => {
   return (
     <Calendar aria-label="Appointment date">
-      <header className="flex items-center h-6">
-        <Button slot="previous">◀</Button>
+      <header className="flex items-center h-6 justify-between w-[21rem]">
+        <Button slot="previous">
+          <FaChevronLeft />
+        </Button>
         <Heading />
-        <Button slot="next">▶</Button>
+        <Button slot="next">
+          <FaChevronRight />
+        </Button>
       </header>
-      <CalendarGrid>
-        {(date) => <CalendarCell date={date} className={"h-12 w-12"} />}
+      <CalendarGrid className="w-[21rem]">
+        <CalendarGridHeader>
+          {(day) => (
+            <CalendarHeaderCell className="w-12 h-12">{day}</CalendarHeaderCell>
+          )}
+        </CalendarGridHeader>
+        <CalendarGridBody className="[&>tr>td]:p-0">
+          {(date) => (
+            <CalendarCell
+              date={date}
+              className="w-12 h-12 flex items-center justify-center"
+            />
+          )}
+        </CalendarGridBody>
       </CalendarGrid>
     </Calendar>
   );
 };
 
 export default BookingCalendar;
+
