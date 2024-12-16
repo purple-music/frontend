ad89b0faf3f8dc955c375127488b7119f5895623
KhoDis <khodzhayarov.a@gmail.com>
2024-12-16 17:37:25 +0300

    feat: add studio and date range selection

---
+++ src/components/atoms/MultiSelectButtonGroup/MultiSelectButtonGroup.tsx
@@ -0,0 +1,77 @@
+import clsx from "clsx";
+import { useState } from "react";
+
+import ButtonBase, { getButtonClasses } from "../ButtonBase/ButtonBase";
+import Typography from "../Typography/Typography";
+
+interface ButtonGroupProps {
+  buttons: { label: string; value: string }[];
+  defaultValues?: string[];
+  onClick?: (selectedValues: string[]) => void;
+}
+
+const MultiSelectButtonGroup = ({
+  buttons,
+  defaultValues = [],
+  onClick,
+}: ButtonGroupProps) => {
+  const [selectedValues, setSelectedValues] = useState<string[]>(defaultValues);
+  const className = getButtonClasses({
+    width: "regular",
+    size: "md",
+    disabled: false,
+    startIcon: false,
+  });
+
+  const handleClick = (value: string) => {
+    let updatedSelectedValues;
+
+    if (selectedValues.includes(value)) {
+      // Remove the value if already selected
+      updatedSelectedValues = selectedValues.filter((v) => v !== value);
+    } else {
+      // Add the value if not selected
+      updatedSelectedValues = [...selectedValues, value];
+    }
+
+    setSelectedValues(updatedSelectedValues);
+
+    if (onClick) {
+      onClick(updatedSelectedValues);
+    }
+  };
+
+  return (
+    <div className="inline-flex rounded-full bg-surface-container-low">
+      {buttons.map(({ label, value }, index) => {
+        const isSelected = selectedValues.includes(value);
+
+        return (
+          <button
+            key={value}
+            onClick={() => handleClick(value)}
+            className={clsx(
+              {
+                "bg-secondary-container text-on-secondary-container enabled:hover:brightness-90 focus:outline-secondary z-10":
+                  isSelected,
+                "bg-transparent text-on-surface enabled:hover:bg-surface-container-high focus:outline-outline":
+                  !isSelected,
+              },
+              className,
+              {
+                "rounded-l-full": index === 0,
+                "rounded-r-full": index === buttons.length - 1,
+              },
+            )}
+          >
+            <Typography variant="label" size="large" component="span">
+              {label}
+            </Typography>
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MultiSelectButtonGroup;
--- src/components/pages/ViewPage/ViewPage.tsx
+++ src/components/pages/ViewPage/ViewPage.tsx
@@ -1,23 +1,54 @@
+import { addDays } from "date-fns";
+import React, { useState } from "react";
+
 import ButtonGroup from "@/components/atoms/ButtonGroup/ButtonGroup";
+import MultiSelectButtonGroup from "@/components/atoms/MultiSelectButtonGroup/MultiSelectButtonGroup";
 import Timetable from "@/components/organisms/Timetable/Timetable";
 import MyTemplate from "@/components/templates/MyTemplate/MyTemplate";
+import { StudioId } from "@/lib/types";
 
 interface ViewPageProps {}
 
+type ButtonGroupButtons<T> = { label: string; value: T }[];
+
 const ViewPage = ({}: ViewPageProps) => {
+  const [selectedStudios, setSelectedStudios] = useState<StudioId[]>([
+    "blue",
+    "orange",
+    "purple",
+  ]);
+  const [days, setDays] = useState<number>(7);
+
+  const today = new Date();
+  const studioButtons: ButtonGroupButtons<StudioId> = [
+    { label: "Purple", value: "purple" },
+    { label: "Orange", value: "orange" },
+    { label: "Blue", value: "blue" },
+  ];
+  const dateRangeButtons: ButtonGroupButtons<number> = [
+    { label: "day", value: 1 },
+    { label: "3 days", value: 3 },
+    { label: "week", value: 7 },
+    { label: "month", value: 30 },
+  ];
+
   return (
     <>
-      <ButtonGroup
-        buttons={[
-          { label: "Home", value: "home" },
-          { label: "Library", value: "library" },
-        ]}
-      />
+      <div className="flex flex-row justify-between w-full">
+        <MultiSelectButtonGroup
+          buttons={studioButtons}
+          onClick={(values) => setSelectedStudios(values as StudioId[])}
+        />
+        <ButtonGroup
+          buttons={dateRangeButtons}
+          onClick={(value) => setDays(value)}
+        />
+      </div>
       <Timetable
-        startDate={new Date("2023-01-01")}
-        endDate={new Date("2023-01-08")}
+        startDate={today}
+        endDate={addDays(today, days)}
         timezone="en-US"
-        studios={["blue", "orange", "purple"]}
+        studios={selectedStudios}
       ></Timetable>
     </>
   );
