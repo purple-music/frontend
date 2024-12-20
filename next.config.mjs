aa322e2c026ffe612ad44a4eb2bbfe5a140881a0
KhoDis <khodzhayarov.a@gmail.com>
2024-12-20 18:33:11 +0300

    fix: add translations for russian

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-import nextIntl from "next-intl/plugin";
+import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = nextIntl();
+const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   output: "standalone",
 };
 
 export default withNextIntl(nextConfig);
-
