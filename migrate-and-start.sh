dddb9429adeb078de81d625a7ddebc971b75e871
KhoDis <khodzhayarov.a@gmail.com>
2024-12-21 16:41:24 +0300

    fix: add studio seeding

--- migrate-and-start.sh
+++ migrate-and-start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 DATABASE_URL="postgresql://purple:music_purple@db:5432/music?sslmode=disable" npx prisma migrate deploy
+DATABASE_URL="postgresql://purple:music_purple@db:5432/music?sslmode=disable" npx prisma db seed
 DATABASE_URL="postgresql://purple:music_purple@db:5432/music?sslmode=disable" node server.js
