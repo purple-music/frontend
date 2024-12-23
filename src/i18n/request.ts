6975a675cd314fbb5f6d1a84ef01dce25dda51bf
KhoDis <khodzhayarov.a@gmail.com>
2024-12-23 16:38:30 +0300

    build: log messages

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -6,10 +6,18 @@ export default getRequestConfig(async ({ requestLocale }) => {
   // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
 
+  console.log("===> request.ts: locale", locale);
+
   // Ensure that a valid locale is used
   if (!locale || !routing.locales.includes(locale as any)) {
+    console.log("===> request.ts: Invalid locale", locale);
     locale = routing.defaultLocale;
   }
+
+  console.log(
+    "importing messages:",
+    (await import(`../../messages/${locale}.json`)).default,
+  );
 
   return {
     locale,
