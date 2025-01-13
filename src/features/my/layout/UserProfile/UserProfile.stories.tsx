14ce283258f51b2766ee0d8d5c87da82883d3bce
KhoDis <khodzhayarov.a@gmail.com>
2025-01-13 16:18:19 +0300

    fix: add missing props on userprofile stories

--- src/features/my/layout/UserProfile/UserProfile.stories.tsx
+++ src/features/my/layout/UserProfile/UserProfile.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Image from "next/image";
 
 import UserProfile from "./UserProfile";
 
@@ -13,17 +12,19 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    profilePicture: (
-      <Image
-        width={64}
-        height={64}
-        layout="fixed"
-        src={"/logo.webp"}
-        alt="Logo"
-        className="h-16 w-16 rounded-full"
-      />
-    ),
-    name: "John Doe",
-    email: "jdoe@me.com",
+    // profilePicture: (
+    //   <Image
+    //     width={64}
+    //     height={64}
+    //     layout="fixed"
+    //     src={"/logo.webp"}
+    //     alt="Logo"
+    //     className="h-16 w-16 rounded-full"
+    //   />
+    // ),
+    // name: "John Doe",
+    // email: "jdoe@me.com",
+    isModalOpen: false,
+    setIsModalOpen: (value: boolean) => console.log(value),
   },
 };
