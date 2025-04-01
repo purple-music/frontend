c6f00ff190f53064344ea8adb71bae01a4ee7558
KhoDis <khodzhayarov.a@gmail.com>
2025-04-01 16:56:07 +0300

    refactor: remove unused code

--- src/features/my/dashboard/DashboardPage/DashboardPage.stories.tsx
+++ src/features/my/dashboard/DashboardPage/DashboardPage.stories.tsx
@@ -1,26 +1,26 @@
-import type { Meta, StoryObj } from "@storybook/react";
-
-import MyTemplate from "@/features/my/layout/MyTemplate/MyTemplate";
-
-import DashboardPage from "./DashboardPage";
-
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
-  title: "pages/DashboardPage",
-  component: DashboardPage,
-  parameters: {
-    layout: "fullscreen",
-  },
-} satisfies Meta<typeof DashboardPage>;
-
-export default meta;
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {
-  args: { date: new Date() },
-  render: (args) => (
-    <MyTemplate page={""}>
-      <DashboardPage {...args} />
-    </MyTemplate>
-  ),
-};
+// import type { Meta, StoryObj } from "@storybook/react";
+//
+// import MyTemplate from "@/features/my/layout/MyTemplate/MyTemplate";
+//
+// import DashboardPage from "./DashboardPage";
+//
+// // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+// const meta = {
+//   title: "pages/DashboardPage",
+//   component: DashboardPage,
+//   parameters: {
+//     layout: "fullscreen",
+//   },
+// } satisfies Meta<typeof DashboardPage>;
+//
+// export default meta;
+// type Story = StoryObj<typeof meta>;
+//
+// export const Default: Story = {
+//   args: {},
+//   render: (args) => (
+//     <MyTemplate page={""}>
+//       <DashboardPage {...args} />
+//     </MyTemplate>
+//   ),
+// };
