4a98d6fb31f7195f2981780fb4e69709f3ab0fae
KhoDis <khodzhayarov.a@gmail.com>
2025-04-21 17:08:41 +0300

    fix: allow telegram image urls

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "avatars.yandex.net",
       },
+      {
+        protocol: "https",
+        hostname: "t.me",
+      },
     ],
   },
 };
